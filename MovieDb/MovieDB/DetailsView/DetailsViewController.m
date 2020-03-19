7109ddf9c3ad218054297cfa2456e5ed298b854c
meduardcn <m.eduardacn@gmail.com>
2020-03-19 16:18:56 -0300

    trying to make a request

--- MovieDb/MovieDB/DetailsView/DetailsViewController.m
+++ MovieDb/MovieDB/DetailsView/DetailsViewController.m
@@ -16,15 +16,37 @@
 @property (weak, nonatomic) IBOutlet UILabel *score;
 @property (weak, nonatomic) IBOutlet UITextView *overviewText;
 
-
 @end
 
 @implementation DetailsViewController
  
 - (void)viewDidLoad {
     [super viewDidLoad];
-    _cardImage.layer.cornerRadius = 10;
+}
 
+-(void)getMovieDetails: (int)  movieId {
+    NSString* mId = [@(movieId) stringValue];
+    NSString *urlString = [NSString stringWithFormat:@"https://api.themoviedb.org/3/movie/%@?api_key=e33d6d132f3f10c719d71ad8b3149066&language=en-US", mId];
+
+    NSURL *url = [NSURL URLWithString: urlString ];
+
+    NSURLSession *session = [NSURLSession sharedSession];
+
+    NSURLSessionDataTask *task = [session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSError *erro = nil;
+        if (task!=nil) {
+           id jsonObject = [NSJSONSerialization
+                                JSONObjectWithData:[task dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingAllowFragments error:&error];
+           if (jsonObject != nil && error == nil) {
+             NSLog(@"Successfully deserialized...");
+           }
+        }
+//        dispatch_sync(dispatch_get_main_queue(),^{
+//            [tanl reloadData];
+//        });
+    }];
+    [task resume];
+    
 }
 
 @end
