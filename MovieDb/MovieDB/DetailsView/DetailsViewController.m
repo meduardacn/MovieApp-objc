5580cd6340146a42c82847ff48d11e6ae53b29db
meduardcn <m.eduardacn@gmail.com>
2020-03-23 12:45:58 -0300

    Network layer working

--- MovieDb/MovieDB/DetailsView/DetailsViewController.m
+++ MovieDb/MovieDB/DetailsView/DetailsViewController.m
@@ -29,15 +29,15 @@
 }
 
 -(void)getMovieDetails: (NSString *)  movieId {
-    [network fetchMovieWithID: movieId withCompletionHandler: ^(NSObject* movie){
-        NSLog(@"%@", moviet.title);
+    [network fetchMovieWithID: movieId withCompletionHandler: ^(Movie* movie){
+        NSLog(@"%@", movie.title);
         dispatch_async(dispatch_get_main_queue(), ^{
             self.movieTitle.text = movie.title;
             self.category.text = movie.genres;
             self.score.text = [NSString stringWithFormat:@"%@",movie.vote_average];
             self.overviewText.text = movie.overview;
         });
-    }]
+    }];
 }
 
 @end
